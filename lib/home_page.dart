3b01ce1f4047068b685d752b92c1dda61d82896c
Abhishek Jain <abhishek.2022@tecorb.co>
2023-05-09 18:54:03 +0530

    screenshot implemented

---
+++ lib/home_page.dart
@@ -0,0 +1,126 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:screenshot/screenshot.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //Create an instance of ScreenshotController
+  ScreenshotController screenshotController = ScreenshotController();
+
+  @override
+  void initState() {
+    // if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Screenshot Flutter"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Screenshot(
+              controller: screenshotController,
+              child: Container(
+                  padding: const EdgeInsets.all(30.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blueAccent, width: 5.0),
+                    color: Colors.amberAccent,
+                  ),
+                  child: Stack(
+                    children: [
+                      Image.asset(
+                        'assets/images/flutter.png',
+                      ),
+                      const Text("This widget will be captured as an image"),
+                    ],
+                  )),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            ElevatedButton(
+              child: const Text(
+                'Capture Above Widget',
+              ),
+              onPressed: () {
+                screenshotController
+                    .capture(delay: const Duration(milliseconds: 10))
+                    .then((capturedImage) async {
+                  ShowCapturedWidget(context, capturedImage!);
+                }).catchError((onError) {
+                  print(onError);
+                });
+              },
+            ),
+            ElevatedButton(
+              child: const Text(
+                'Capture An Invisible Widget',
+              ),
+              onPressed: () {
+                var container = Container(
+                    padding: const EdgeInsets.all(30.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blueAccent, width: 5.0),
+                      color: Colors.redAccent,
+                    ),
+                    child: Stack(
+                      children: [
+                        Image.asset(
+                          'assets/images/flutter.png',
+                        ),
+                        Text(
+                          "This is an invisible widget",
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                      ],
+                    ));
+                screenshotController
+                    .captureFromWidget(
+                        InheritedTheme.captureAll(
+                            context, Material(child: container)),
+                        delay: const Duration(seconds: 1))
+                    .then((capturedImage) {
+                  ShowCapturedWidget(context, capturedImage);
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<dynamic> ShowCapturedWidget(
+      BuildContext context, Uint8List capturedImage) {
+    return showDialog(
+      useSafeArea: false,
+      context: context,
+      builder: (context) => Scaffold(
+        appBar: AppBar(
+          title: const Text("Captured widget screenshot"),
+        ),
+        body: Center(
+            child: capturedImage != null
+                ? Image.memory(capturedImage)
+                : Container()),
+      ),
+    );
+  }
+
+  // _saved(File image) async {
+  //   // final result = await ImageGallerySaver.save(image.readAsBytesSync());
+  //   print("File Saved to Gallery");
+  // }
+}
